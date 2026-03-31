a570642632a67234fc8bd11363527685cabf2459
Athirah Afwina <athirahafwina@gmail.com>
2026-03-31 16:57:09 -0300

    part 1.3-1.5

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -27,9 +27,7 @@
 const Header = (props) => {
 
   return (
-    <>
       <h1>{props.course}</h1>
-    </>
   )
 }
 
@@ -67,18 +65,20 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const content = [
-    {name:'Fundamentals of React', exercises:10},
-    {name:'Using props to pass data', exercises:7},
-    {name:'State of a component', exercises:14},
-  ]
+  const course ={
+      name:'Half Stack application development',
+      parts: [
+          {name:'Fundamentals of React', exercises:10},
+          {name:'Using props to pass data', exercises:7},
+          {name:'State of a component', exercises:14},
+      ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content content={content} />
-      <Total content={content} />
+      <Header course={course.name} />
+      <Content content={course.parts} />
+      <Total content={course.parts} />
     </div>
   )
 }
