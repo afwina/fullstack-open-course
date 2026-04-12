c78dd8cf593130d77a5ba5daf01e5a18c626ebd8
Athirah Afwina <athirahafwina@gmail.com>
2026-04-12 19:48:34 -0300

    3.9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,12 +4,11 @@ const morgan = require('morgan')
 const app = express()
 const PORT = 3001
 
-
-
 app.use(express.json())
 
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(express.static('dist'))
 
 let persons = [
     {
